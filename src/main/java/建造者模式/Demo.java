af37b5d7d368f704273b0ffdcb8ead86a9aef575
rockyshen <875430544@qq.com>
2025-09-18 10:02:51 +0800

    feat: 建造者模式：1、构造函数私有化；2、属性设为final；3、利用中间对象builder传递属性；4、支持链式调用

--- src/main/java/建造者模式/Demo.java
+++ src/main/java/建造者模式/Demo.java
@@ -1,14 +1,26 @@
 package 建造者模式;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author rockyshen
  * @date 2025/9/17 11:29
  */
 public class Demo {
     public static void main(String[] args) {
-        User.Builder builder = new User.Builder();
-        builder.name("tom");
-        builder.age(11);
-        User user = builder.build();
+        // 传统new方法
+//        User.Builder builder = new User.Builder();
+//        builder.name("tom");
+//        builder.age(11);
+//        User user = builder.build();
+//        System.out.println(user);
+
+        // 建造者模式，链式调用
+        // 本质上，操作的是中间对象 builder，避免直接操作user
+        User user = User.builder().name("tom").age(11).build();
+        // 联想到stream流,stream()相当于builder的中间对象，map、filter相当于name、age方法中间操作、collect相当于build()中止操作
+//        myList.stream().map(String::valueOf).filter(e -> {e.length() > 1}).collect(Collectors.toList());
     }
 }
--- src/main/java/建造者模式/User.java
+++ src/main/java/建造者模式/User.java
@@ -1,41 +1,62 @@
 package 建造者模式;
 
 import lombok.Data;
+import lombok.Getter;
 
 /**
  * @author rockyshen
  * @date 2025/9/17 11:28
  */
-@Data
+@Getter   // 不提供setter方法，对外不提供修改属性的方法
 public class User {
-    private String name;
-    private Integer age;
+    private final String name;   // 对象属性field一旦被声明为final，表示一旦赋某个值，就不能改了
+    private final Integer age;
+
+    // 私有化User构造方法，不允许外部使用new User创建，只能用Builder
+    private User(Builder builder){
+        this.name = builder.name;
+        this.age = builder.age;
+    }
+
+    // 静态方法
+    public static Builder builder(){
+        return new Builder();
+    }
 
     // 静态内部类，建造者
     public static class Builder {
         private String name;
         private Integer age;
 
-        public void name(String name){
+        // 构造函数私有化
+        private Builder() {}
+
+        public Builder name(String name){
             this.name = name;
+            // 链式调用
+            return this;
         }
 
-        public void age(int age){
+        public Builder age(int age){
             this.age = age;
+            return this;
         }
 
         public User build() {
-            User user = new User();
-            user.setName(this.name);
-            user.setAge(this.age);
+            // 由于User的构造函数是私有化的，且属性是final必须new对象的时候提供值，我们通过传递builder对象
+            // 将user需要的属性，通过builder身上的属性传递给user
+            User user = new User(this);
+//            user.name = this.name;    // 把builder的name属性，传递给user，因为user对外不提供setter方法了
+//            user.age = this.age;
 
-            // 建造user对象时，引入一段奇葩的校验逻辑
+            // 【自定义创建实例时，检查属性的特定逻辑】建造user对象时，引入一段奇葩的校验逻辑
             if(age < 10 && "Tom".equals(name)){
                 throw new IllegalArgumentException("");
             }
             if(age > 10 && "Jerry".equals(name)){
                 throw new IllegalArgumentException("");
             }
+
             return user;
         }
     }
